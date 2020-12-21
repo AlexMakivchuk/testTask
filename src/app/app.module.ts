80c9c45ae956a66b5e08c3b812fff559294defed
Олександр <alexmakivchuk@gmail.com>
2020-12-21 12:53:15 +0200

    clear imports in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {TestTaskComponent} from './components/test-task/test-task.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
 import {SharedModule} from './shared/shared/shared.module';
 
 @NgModule({
